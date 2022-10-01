3bb99c01da5f5a726d7604ac1a5e7e8735e2e403
Apanasenko Andrey <aw_apanasenko@mail.ru>
2022-10-01 15:50:13 +0000

    add webmozart library in project

--- tests/StringUtilsTest.php
+++ tests/StringUtilsTest.php
@@ -1,18 +1,17 @@
 <?php
 
-/*
-Нажмите кнопку run чтобы запустить тесты.
-Попробуйте изменять код функции / тестов, запуская проверки заново.
-*/
+require("vendor/autoload.php");
 
-require("src/StringUtils.php");
+use Webmozart\Assert\Assert;
+use function StringUtils\capitalize;
 
-if (StringUtils\capitalize('hello') !== 'Hello') {
-    throw new \Exception('Функция работает неверно!');
-}
+// eq означает equal
 
-if (StringUtils\capitalize('') !== '') {
-    throw new \Exception('Функция работает неверно!');
-}
+Assert::eq(capitalize(''), '');
+// Первый параметр actual – то, что пришло
+// Второй параметр expected – то, что ожидает тест
+// Правильный порядок аргументов имеет большое значение при анализе ошибки
+
+Assert::eq(capitalize('hello'), 'Hello');
 
 echo 'Все тесты пройдены!';
